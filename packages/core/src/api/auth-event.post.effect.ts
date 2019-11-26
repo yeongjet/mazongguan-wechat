c6b97b98c849d8a106000eaaae7ae284f7fdd7b1
yeongjet <625436194@163.com>
2019-11-26 23:33:00 +0800

    update

--- packages/core/src/api/auth-event.post.effect.ts
+++ packages/core/src/api/auth-event.post.effect.ts
@@ -13,35 +13,6 @@ const validator = validator$({
     })
 })
 
-const { xml: { Encrypt: stringEncrypted} } : {
-    xml:{
-        Encrypt: string
-    }
-} = await xml2js.parseStringPromise(XMLEncrypted, {explicitArray: false})
-const md = forge.md.sha1.create();
-md.update([AppParam.token, URLParam.nonce, URLParam.timestamp, stringEncrypted].sort().join(''), 'utf8')
-if(md.digest().toHex() !== URLParam.msg_signature) {
-    console.log('签名不正确')
-} else {
-    console.log('签名正确')
-}
-const bufferEncrypted = forge.util.createBuffer(forge.util.decode64(stringEncrypted), 'raw')
-const AESKey = forge.util.decode64(`${AppParam.encodingAESKey}=`)
-const decipher = forge.cipher.createDecipher('AES-CBC', AESKey)
-decipher.start({iv: AESKey.substr(0, 16)})
-decipher.update(bufferEncrypted);
-decipher.finish()
-const hexStringDecrypted = decipher.output.toHex()
-const randomNumber = parseInt(hexStringDecrypted.substr(0, 16 * 8 / 4), 16)
-const xmlLength = parseInt(hexStringDecrypted.substr(16 * 8 / 4, 4 * 8 / 4), 16)
-const xmlContent = forge.util.hexToBytes(hexStringDecrypted.substr(20 * 8 / 4, xmlLength * 2))
-const appid = forge.util.hexToBytes(hexStringDecrypted.substr(20 * 8 / 4 + xmlLength * 2))
-if(appid !== AppParam.appid){
-    console.log('appid不匹配')
-}else{
-    console.log('appid匹配')
-}
-
 export const createBatch$ = EffectFactory.matchPath('wechat-third/auth-event')
     .matchType('POST')
     .use(req$ =>
